20c1f76316d680db19e168a2789b6b994339d337
johnstudarus <john@jhlconsulting.com>
2017-06-01 16:19:47 -0700

    Create enable-port-forwarding.sh

---
+++ enable-port-forwarding.sh
@@ -0,0 +1,5 @@
+# setup port forwarding for access to the VMs created
+
+socat tcp-listen:8001,reuseaddr,fork tcp:10.20.0.2:8000 &
+socat tcp-listen:8002,reuseaddr,fork tcp:172.16.0.3:80 &
+socat tcp-listen:8003,reuseaddr,fork tcp:172.16.0.131:22 &
