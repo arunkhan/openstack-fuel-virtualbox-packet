bb9aefa4e75070e9b833adee5656536216475a9b
johnstudarus <john@jhlconsulting.com>
2017-06-01 16:36:03 -0700

    Update enable-port-forwarding.sh

--- enable-port-forwarding.sh
+++ enable-port-forwarding.sh
@@ -3,3 +3,6 @@
 socat tcp-listen:8001,reuseaddr,fork tcp:10.20.0.2:8000 &
 socat tcp-listen:8002,reuseaddr,fork tcp:172.16.0.3:80 &
 socat tcp-listen:8003,reuseaddr,fork tcp:172.16.0.131:22 &
+
+
+IP=`hostname -I | cut -d' ' -f 1`
